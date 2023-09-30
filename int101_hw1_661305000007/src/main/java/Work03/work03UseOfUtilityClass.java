db669b51bd76c617059321f3d41c754d4342c457
View <kittikan.weel@mail.kmutt.ac.th>
2023-09-30 21:08:25 +0700

    all i need

---
+++ int101_hw1_661305000007/src/main/java/Work03/work03UseOfUtilityClass.java
@@ -0,0 +1,10 @@
+package Work03;
+
+import static Work03.MyUtil.average;
+import static Work03.MyUtil.calculateBMI;
+public class work03UseOfUtilityClass {
+    public void main(String[] args){
+        System.out.println(calculateBMI(165,58));
+        System.out.println(average(4,5,6));
+    }
+}
